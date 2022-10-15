070b2d5b3d81de75d7f006d7e3f2d2b4cd869cd6
Yu Wang <70514121+wangyu-sd@users.noreply.github.com>
2022-10-15 21:15:31 +0800

    Update train_for_uspto.sh

--- rxn_yld/scripts/train_for_uspto.sh
+++ rxn_yld/scripts/train_for_uspto.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 python entry.py\
-  --batch_size 128 \
+  --batch_size 256 \
   --acc_batches 1 \
-  --d_model 256 \
-  --dim_feedforward 512 \
+  --d_model 128 \
+  --dim_feedforward 256 \
   --epochs 2000 \
   --dropout 0.2 \
-  --warmup_updates 2000 \
-  --tot_updates 1000000 \
+  --warmup_updates 200000 \
+  --tot_updates 100000000 \
   --dataset data/uspto \
   --known_rxn_cnt \
   --norm_first \
-  --nhead 32 \
-  --p_layer 4 \
-  --r_layer 4 \
-  --output_layer 4 \
+  --nhead 8 \
+  --p_layer 3 \
+  --r_layer 3 \
+  --output_layer 3 \
   --seed 123 \
-  --cuda 1 \
+  --cuda 1,2 \
   --max_single_hop 4 \
-  --log_dir debug \
+  --log_dir tb_lgs \
   --not_fast_read
